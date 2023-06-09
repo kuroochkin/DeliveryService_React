15cb831df07ed425ccee7700c20184c59624abdb
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-09 23:15:52 +0300

    Список заказов курьера по их статусу

---
+++ src/pages/courier/allOrdersCourierByStatus/AllOrdersCourierByStatusPage.js
@@ -0,0 +1,96 @@
+import useOrderService from "../../../services/OrderService";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow, Paper} from "@mui/material";
+import { styled } from '@mui/material/styles';
+import './allOrdersCourierByStatusPage.scss';
+
+
+const AllOrdersCourierByStatus = () => {
+    
+    const [data, setData] = useState(null);
+    const [Button, setButton] = useState(null);
+
+    const {status} = useParams();
+
+    const {getOrdersByCourierByStatus} = useOrderService();
+
+    useEffect(() => {
+        getOrdersByCourierByStatus(status)
+            .then(data => setData(data))
+	}, []);
+
+    console.log(data);
+
+
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+          background: "black",
+          color: "white",
+          fontSize: 20,
+          fontFamily: 'Nunito',
+          textAlign: 'center'
+        },
+        [`&.${tableCellClasses.body}`]: {
+          fontSize: 18,
+          fontFamily: 'Nunito',
+          textAlign: 'center'
+        },
+      }));
+      
+      const StyledTableRow = styled(TableRow)(({ theme }) => ({
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.action.hover,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+          border: 0,
+        },
+      }));
+
+    const renderItems = (data) => {
+        return (
+            <>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <StyledTableRow>
+                            <StyledTableCell align="center">Дата создания</StyledTableCell>
+                            <StyledTableCell align="center">Статус</StyledTableCell>
+                            <StyledTableCell align="center">Описание</StyledTableCell>
+                            <StyledTableCell align="center">Заказчик</StyledTableCell>
+                            
+
+                        </StyledTableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.orders.map((order) => (
+                            <StyledTableRow >
+                                <StyledTableCell align="center">{order.created.slice(0,10) + ' '+ order.created.slice(11,16)}</StyledTableCell>
+                                <StyledTableCell align="center">Ожидает курьера</StyledTableCell>
+                                <StyledTableCell align="center">{order.description}</StyledTableCell>
+                                <StyledTableCell align="center">{order.customer.firstName + ' ' + order.customer.lastName}</StyledTableCell>
+                            </StyledTableRow>))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </>
+
+        )
+    }
+
+    let items;
+
+    if(data !== null){
+        items = renderItems(data);
+    }
+
+    return (
+        <div className='order_list__container'>
+            {data !== null ? items : null}
+        </div>
+        
+    )
+}
+
+export default AllOrdersCourierByStatus;
