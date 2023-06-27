ff5eba87618448c00e7e494218febab899727349
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-27 17:07:45 +0300

    Поправки в HomePage

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -6,20 +6,6 @@ const HomePage = ({cartItems, setCartItems}) => {
 
     return (  
         <div>
-            <div className="button input">
-                    <Button variant="contained" size="medium" type="submit" 
-                        onClick={() => navigateOrders('Create')}>Созданные заказы
-                    </Button>
-                    
-                    <Button variant="contained" size="medium" type="submit" 
-                    onClick={() => navigateOrders('Progress')}>Заказы в пути
-                    </Button>
-
-                    <Button variant="contained" size="medium" type="submit" 
-                    onClick={() => navigateOrders('Complete')}>Завершенные заказы
-                    </Button>
-                </div>    
-                <p></p>
             <ProductList cartItems={cartItems} setCartItems={setCartItems}/>
         </div>  
     )
