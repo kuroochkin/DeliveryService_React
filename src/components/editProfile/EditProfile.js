cc7c4084c22c90bcca8bbc628d9d8b43733a5c00
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-07-20 19:34:34 +0300

    Скрыт пароль

--- src/components/editProfile/EditProfile.js
+++ src/components/editProfile/EditProfile.js
@@ -1,11 +1,11 @@
 import { useState, useEffect} from "react";
-import { Button } from "@mui/material";
+import { Button, Checkbox} from "@mui/material";
 import useOrderService from "../../services/OrderService";
 import "./editProfile.scss";
 
 const EditProfile = ({setToken}) => {
 
-    const [data, setData] = useState();
+    const [data, setData] = useState(null);
 
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
@@ -17,13 +17,15 @@ const EditProfile = ({setToken}) => {
     const [itsOk, setItsOk] = useState(false);
     const [isRequest, setIsRequest] = useState(false);
 
-    
+    const [typePass, setTypePass] = useState("password");
+
     const {getCustomerById, registerUser} = useOrderService();
 
     useEffect(() => {
         getCustomerById()
             .then(data => setData(data));
     }, []);
+
 
     console.log(data);
 
@@ -54,50 +56,73 @@ const EditProfile = ({setToken}) => {
         setIsRequest(false);
     };
 
+    const seePass = () => {
+        if(typePass === "text"){
+            setTypePass("password");
+        } else {
+            setTypePass("text");
+        }
+    };
+
+    const renderItems = (data) => {
+        return (
+            <div className="cont">
+                <form onSubmit={handleSubmit}> 
+                    <div className="text input">
+                        <label>
+                            <p>Имя</p>
+                            <input type="text" placeholder={data.firstName} onChange={e => setFirstName(e.target.value)}/>
+                        </label>
+                    </div>
+                    <div className="text input">
+                        <label>
+                            <p>Фамилия</p>
+                            <input type="text" placeholder={data.lastName} onChange={e => setLastName(e.target.value)}/>
+                        </label>
+                    </div>
+                    <div className="email input">
+                        <label>
+                            <p>Почта</p>
+                            <input type="text" placeholder={data.email} onChange={e => setEmail(e.target.value)}/>
+                        </label>
+                    </div>
+                    <div className="password input">
+                        <label>
+                            <p>Пароль</p>
+                            <input type={typePass} disabled={true} value={data.password} onChange={e => setPassword(e.target.value)}/>
+                        </label>
+                        
+                    </div>
+                    <Checkbox onChange={seePass}/>
+                    <div className="text input">
+                        <label>
+                            <p>Номер телефона</p>
+                            <input type="text" disabled={true} value={data.phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
+                        </label>
+                    </div>
+                    <div className="text input">
+                        <label>
+                            <p>Город</p>
+                            <input type="text" value={data.city} onChange={e => setCity(e.target.value)}/>
+                        </label>
+                    </div>
+                    <div className="button input">
+                        <Button variant="contained" size="medium" type="submit">Далее</Button>
+                    </div>   
+                </form>
+            </div>
+        )
+    }
+    let items;
+
+    if(data !== null){
+        items = renderItems(data);
+    }
+
     return (
-        <div className="cont">
-            <form onSubmit={handleSubmit}> 
-                <div className="text input">
-                    <label>
-                        <p>Имя</p>
-                        <input type="text" value={data.firstName} onChange={e => setFirstName(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="text input">
-                    <label>
-                        <p>Фамилия</p>
-                        <input type="text" value={data.lastName} onChange={e => setLastName(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="email input">
-                    <label>
-                        <p>Почта</p>
-                        <input type="text" value={data.email} onChange={e => setEmail(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="password input">
-                    <label>
-                        <p>Пароль</p>
-                        <input type="password" value={data.password} onChange={e => setPassword(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="text input">
-                    <label>
-                        <p>Номер телефона</p>
-                        <input type="text"  value={data.phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="text input">
-                    <label>
-                        <p>Город</p>
-                        <input type="text" value={data.city} onChange={e => setCity(e.target.value)}/>
-                    </label>
-                </div>
-                <div className="button input">
-                    <Button variant="contained" size="medium" type="submit">Далее</Button>
-                </div>   
-            </form>
-        </div>
+        <div className='cont'>
+            {data !== null ? items : null}
+        </div> 
     )
 }
 
